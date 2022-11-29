e7c78ab95c265938587ac23744a24a45fa0ff5c4
Adam <adamgrabarczyk.gda@wp.pl>
2022-11-29 20:36:57 +0100

    inintial commit

---
+++ src/views/MainView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main-component />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import MainComponent from "../components/MainComponent.vue";
+
+export default defineComponent({
+  name: "MainView",
+
+  components: {
+    MainComponent,
+  },
+});
+</script>
