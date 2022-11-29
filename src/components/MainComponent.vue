e0b5a11ba75beea5abd44c04046a5175d151fa4f
Adam <adamgrabarczyk.gda@wp.pl>
2022-11-29 23:34:08 +0100

    display currency names after hallo click

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,10 +1,43 @@
 <template>
   <div id="container">
-    <p class="text">Hallo</p>
+    <p class="text" v-on:click="handleInput">Hallo</p>
+    <ul>
+      <li>
+        <p class="text" v-for="item in results" :key="item.code">
+          {{ results != [] ? item.currency : "" }}
+        </p>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
+import { onMounted } from "vue";
+import axios from "axios";
+
+const Api = "http://api.nbp.pl/api/exchangerates/tables/b";
 export default {
-  //
+  name: "nbpCurrency",
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    handleInput: function () {
+      axios
+        .get(Api)
+        .then((response) => {
+          console.log(response.data[0].rates[0].code);
+          this.results = response.data[0].rates;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    alert: function () {
+      alert("blah");
+    },
+  },
 };
 </script>
